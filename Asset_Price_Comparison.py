3a47e633394f30c2dd90c57b56a5a2d9453123b7
shankarsc <60320843+shankarsc@users.noreply.github.com>
2020-09-09 15:43:37 +0800

    Add files via upload
    Added Pearson correlation and QuantDare correlation calculation for comparison between two stationary time-series data.

--- Asset_Price_Comparison.py
+++ Asset_Price_Comparison.py
@@ -6,30 +6,103 @@ import datetime as dt
 import matplotlib.pyplot as plt
 
 def asset_comparison(ticker, startdate, enddate):
-    """
-    Returns the adjusted daily close prices of the assets using the YF_Pruner module.
-    Missing price data is forward-filled.
-    """
-    data = yfp.get_daily_adj_close(ticker, startdate, enddate)
-    data = data.ffill(axis=0)
-    return data
+      """
+      Returns the adjusted daily close prices of the assets using the YF_Pruner module.
+      Missing price data is forward-filled.
 
-def comparison_plot(data):
-    """
-    Plots the prices of the assets.
-    """
-    fig, ax = plt.subplots(figsize=(12,6))
+      Parameters
+      ----------
+      data :   'pd.DataFrame'
+            The DataFrame containing prices for each series
+      startdate :   'datetime'
+            The start date for asset prices returned from Yahoo Finance!
+      enddate :   'datetime'
+            The end date for asset prices returned from Yahoo Finance!
+      """
+      data = yfp.get_daily_adj_close(ticker, startdate, enddate)
+      data = data.ffill(axis=0)
+      return data
 
-    # Plots the price data from the first ticker
-    ax.plot(data.index, data['Adj Close'].iloc[0:, 0], color='blue')
-    ax.set_ylabel('Price ($)')
-    ax.set_xlabel('Date')
-    ax.legend([data['Adj Close'].columns[0]], loc='upper center')
+def price_comparison_plot(data):
+      """
+      Plots the prices of the assets.
 
-    # Plots the price data from the second ticker
-    ax2 = ax.twinx()
-    ax2.set_ylabel('Price ($)')
-    ax2.plot(data.index, data['Adj Close'].iloc[0:, 1], color='orange')
-    ax2.legend([data['Adj Close'].columns[1]], loc='upper right')
+      Parameters
+      ----------
+      data :   'pd.DataFrame'
+            The DataFrame containing prices for each series
+      """
+      fig, ax = plt.subplots(figsize=(12,6))
 
-    plt.show()
+      # Plots the price data from the first ticker
+      ax.plot(data.index, data.iloc[0:, 0], color='blue', label=data.columns[0])
+      ax.set_ylabel(data.columns[0])
+      ax.set_xlabel('Date')
+      ax.legend([data.columns[0]], loc='upper left')
+
+      # Plots the price data from the second ticker
+      ax2 = ax.twinx()
+      ax2.set_ylabel(data.columns[1])
+      ax2.plot(data.index, data.iloc[0:, 1], color='orange', label=data.columns[1])
+      ax2.legend([data.columns[1]], loc='lower left')
+
+      plt.title('Price Movement of %s and %s' % (data.columns[0], data.columns[1]))
+      plt.show()
+
+def scatter_plot(data, y_ts, x_ts):
+      """
+      Plot a scatter plot of both time series for the provided DataFrames.
+
+      Parameters
+      ----------
+      data  :   'pd.DataFrame'
+            The DataFrame containing prices for each series
+      ts1 :   'str'    
+            The first time series column name -  tuple of multi-index column e.g. [('Adj Close', 'COLUMN_NAME')]
+      ts2 :   'str'
+            The second time series column name -  tuple of multi-index column e.g. [('Adj Close', 'COLUMN_NAME')]
+      """
+      plt.figure(figsize=(12,6))
+      plt.xlabel('%s Price ($)' % data[x_ts].name)
+      plt.ylabel('%s Price ($)' % data[y_ts].name)
+      plt.title('%s and %s Price Scatterplot' % (data[y_ts].name, data[x_ts].name))
+      plt.scatter(data[x_ts], data[y_ts])
+      plt.show()
+
+def pearson_corr(data):
+      """
+      Returns the Pearson correlation for two (stationary) time series data. 
+      More appropriately used over asset price than returns.
+
+      Parameters
+      ----------
+      data :   'pd.DataFrame'
+            The DataFrame containing prices for each series
+      
+      Return
+      ------
+      Pearson correlation between the time series.
+      """
+      numer =  ((data.iloc[0:, 0] - data.iloc[0:, 0].mean()) * (data.iloc[0:, 1] - data.iloc[0:, 1].mean())).sum()
+      denom = (((data.iloc[0:, 0] - data.iloc[0:, 0].mean())**2).sum() * ((data.iloc[0:, 1] -  data.iloc[0:, 1].mean())**2).sum())**0.5
+      print('Pearson correlation of %s and %s: %s' % (data.columns[0], data.columns[1], round((numer/denom), 5)))
+
+def qd_corr(data):
+      """
+      Returns the QuantDare correlation for two (stationary) time series data.
+      Difference with the Pearson correlation is the mean of the series 
+      is removed from the calculation.
+      More appropriately used over asset returns than price.
+
+      Parameters
+      ----------
+      data :   'pd.DataFrame'
+            The DataFrame containing prices for each series
+      
+      Return
+      ------
+      QuantDare correlation between the time series.
+      """
+      numer = (data.iloc[0:, 0]*data.iloc[0:, 1]).sum()
+      denom = (((data.iloc[0:, 0])**2).sum() * ((data.iloc[0:, 1])**2).sum())**0.5
+      print('QuantDare correlation of %s and %s: %s' % (data.columns[0], data.columns[1], round((numer/denom), 5)))
